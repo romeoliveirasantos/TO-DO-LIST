8ad151f827e20186f8c6a7e0126e167ecd0d957e
Romário Oliveira <rjob.oliveira@gmail.com>
2022-12-13 15:49:01 -0300

    Create functions and responsive media querys

---
+++ index.js
@@ -0,0 +1,26 @@
+const textHeader = document.querySelector('.p-header');
+const portuguese = document.querySelector('.portuguese');
+const english = document.querySelector('.english');
+const textMain = document.querySelector('.p-section-main');
+const buttonCreate = document.querySelector('.button-createList');
+const textFooter = document.querySelector('.p-footer');
+const textFooterAnchor = 'DESENVOLVIDO POR <a href="https://www.linkedin.com/in/rom%C3%A1rio-oliveira-b9022a1b7/" target="_blank">R.OlIVEIRA</a>'
+
+portuguese.addEventListener('click', changePortuguese);
+english.addEventListener('click', changeEnglish);
+
+
+function changePortuguese(){
+  textHeader.textContent = "ESCOLHA O IDIOMA";
+  buttonCreate.textContent = "CRIAR LISTA";
+  textMain.textContent = "Crie listas de tudo que você tem a fazer, otimize seu tempo e organize seu dia."
+  textFooter.textContent = "DESENVOLVIDO POR R.OlIVEIRA."
+}
+
+function changeEnglish(){
+  textHeader.textContent = "CHOSE LANGUAGE";
+  buttonCreate.textContent = "CREATE LIST";
+  textMain.textContent = "Create lists of everything you have to do, optimize your time, and organize your day."
+  textFooter.textContent = "DEVELOPED BY R.OLIVEIRA."
+}
+
---
+++ listPage.js
@@ -0,0 +1,67 @@
+const input = document.querySelector('.input');
+const div = document.querySelector('.list-div')
+const createDivtask = document.createElement('div');
+const buttonCreateList = document.querySelector('.button-createList');
+
+const validateInput = () => {
+  return input.value.trim().length > 0;
+}
+
+const handleAddTask = () =>{
+  const inputIsValid = validateInput();
+
+  if(!inputIsValid) {
+   return  input.classList.add("error");
+  }
+
+  const taskItemContainer = document.createElement('div');
+  taskItemContainer.classList.add("task")
+
+  const taskText = document.createElement('p')
+  taskText.innerText = input.value;
+
+  
+
+  const taskDivIcon = document.createElement('div');
+
+  const completeItem = document.createElement("i")
+  completeItem.classList.add("fa-solid")
+  completeItem.classList.add("fa-check")
+  completeItem.classList.add("completeIcon")
+
+  completeItem.addEventListener('click',() => handleClick(taskText))
+
+  const deleteItem = document.createElement("i")
+  deleteItem.classList.add("far");
+  deleteItem.classList.add("fa-trash-alt");
+  deleteItem.classList.add("deleteIcon")
+
+  deleteItem.addEventListener('click', () => handleDeleteClick(taskItemContainer))
+
+  taskItemContainer.appendChild(taskText);
+  taskItemContainer.appendChild(taskDivIcon)
+  taskDivIcon.appendChild(completeItem);
+  taskDivIcon.appendChild(deleteItem);
+
+  div.appendChild(taskItemContainer);
+}
+const handleClick = (taskText) => {
+  taskText.style.textDecoration = "line-through"
+  taskText.style.color = "green"
+  taskItemContainer.style.backgroundColor = "green"
+}
+const handleDeleteClick = (taskItemContainer) => {
+  taskItemContainer.remove()
+}
+const handleInputChange = () =>{
+  const inputIsValid = validateInput();
+
+  if(inputIsValid) {
+    return input.classList.remove("error");
+  }
+}
+
+buttonCreateList.addEventListener('click', () => handleAddTask())
+
+input.addEventListener('change', () => handleInputChange())
+
